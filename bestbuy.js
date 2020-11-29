51bebb117b0afcc9091cfff4366d5d34ffe85b9a
quivsoth <phillip.knezevich@gmail.com>
2020-11-29 04:07:27 -0500

    Mods edited

---
+++ bestbuy.js
@@ -0,0 +1,31 @@
+var webdriver = require('selenium-webdriver');
+var message = "gm";
+
+//var driver = new RemoteWebDriver(new Uri(<RemoteUrl>), options);
+
+console.log('my node js in a docker stuff stuff stuff0');
+var driver = new webdriver.Builder().
+    withCapabilities(webdriver.Capabilities.chrome())
+    .usingServer('http://chrome:4444/wd/hub')
+    .build();
+
+    async function Run(){
+        try{
+            driver.get('https://classroom.google.com/u/0/h');
+            await driver.wait(webdriver.until.elementLocated(webdriver.By.id("identifierId")), 10000);
+            // User Name
+            await driver.findElement(webdriver.By.id("identifierId")).sendKeys("TODO");
+            await driver.findElement(webdriver.By.id("identifierId")).sendKeys(webdriver.Key.ENTER);
+            await driver.sleep(1000);
+        }
+        catch(err){
+            handleFailure(err, driver)
+        }
+    }
+
+    Run();
+
+    function handleFailure(err, driver) {
+        console.error('Something went wrong!\n', err.stack, '\n');
+        driver.quit();
+    }
