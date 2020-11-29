ea25287dd8e3cc206c78c98623f1457d47ae3355
quivsoth <phillip.knezevich@gmail.com>
2020-11-29 03:53:47 -0500

    Mods edited

--- startup.sh
+++ startup.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/bash
-java -jar /opt/selenium/selenium-server-standalone.jar &
-P1=$!
-#node /usr/src/app/inventory-manager/index.js &
-#P2=$!
-wait $P1 #$P2
-
+#java -jar /opt/selenium/selenium-server-standalone.jar &
+#P1=$!
+node /usr/src/app/inventory-manager/index.js &
+P2=$!
+#wait $P1 $P2
+wait $P2
 
 # node /usr/src/app/inventory-manager/index.js && java -jar /opt/selenium/selenium-server-standalone.jar
